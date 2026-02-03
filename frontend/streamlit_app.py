7a8c88387abc83904956ce07e0d246dd8d9b36aa
Miran <miran.asanin@gmail.com>
2026-02-03 09:24:56 +0100

    update for api_port

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import requests
 from typing import Dict, Any
 
-BACKEND_URL = "http://46.225.15.6/api/latest"
+BACKEND_URL = "http://46.225.15.6:5000/api/latest"
 
 streamlit.title("Slovenia air quality measurements")
 
